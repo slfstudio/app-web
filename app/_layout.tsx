46e81fa16b36c05313e2e4ce2d4acb1ed525ca3f
Dylan <dyl.blink.1.8.2@gmail.com>
2024-10-30 10:14:12 -0500

    fonts and componentes tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,7 +14,18 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'Inter-Black': require('../assets/fonts/Inter/Inter-Black.ttf'),
+    'Inter-Bold': require('../assets/fonts/Inter/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('../assets/fonts/Inter/Inter-ExtraBold.ttf'),
+    'Inter-ExtraLight': require('../assets/fonts/Inter/Inter-ExtraLight.ttf'),
+    'Inter-Light': require('../assets/fonts/Inter/Inter-Light.ttf'),
+    'Inter-Medium': require('../assets/fonts/Inter/Inter-Medium.ttf'),
+    'Inter-Regular': require('../assets/fonts/Inter/Inter-Regular.ttf'),
+    'Inter-SemiBold': require('../assets/fonts/Inter/Inter-SemiBold.ttf'),
+    'Inter-Thin': require('../assets/fonts/Inter/Inter-Thin.ttf'),
   });
+
+
 
   useEffect(() => {
     if (loaded) {
